b8db3292dea704d55bc8adb6db807a19bfd02c09
William Candillon <wcandillon@gmail.com>
2022-07-07 22:35:23 +0200

    :wrench:

--- src/CRT/CRT.tsx
+++ src/CRT/CRT.tsx
@@ -1,14 +1,86 @@
 import { Group, Fill, ColorMatrix, Mask } from "@shopify/react-native-skia";
 import type { ReactNode } from "react";
+import { useCurrentFrame } from "remotion";
 
-import { RemotionCanvas } from "../components";
+import { RemotionCanvas, useLoop } from "../components";
 import { CANVAS } from "../components/Theme";
 
 import { Tile } from "./Tile";
 import { PhosphorDot, TILE } from "./PhosphorDot";
 import { Reference } from "./Reference";
+import { Refresh } from "./Refresh";
 
 const { width, height, center } = CANVAS;
+
+interface CRTProps {
+  children: ReactNode | ReactNode[];
+}
+
+const CRTEffect = ({ children }: CRTProps) => {
+  const frame = useCurrentFrame();
+  const duration = 30 * 3;
+  const iteration = Math.floor(frame / duration);
+  const progress = (frame % duration) / duration;
+  const revert = iteration % 2;
+  console.log({ iteration, revert });
+  return (
+    <RemotionCanvas
+      typefaces={[require("./assets/PublicPixel.ttf")]}
+      width={width}
+      height={height}
+    >
+      <Refresh revert={revert} progress={progress}>
+        <Fill color="black" />
+        <Group>
+          <Mask
+            mask={
+              <Tile rect={TILE}>
+                <PhosphorDot channel="r" />
+              </Tile>
+            }
+          >
+            <Group>
+              <ColorMatrix matrix={filterColor(1, 0, 0)} />
+              {children}
+            </Group>
+          </Mask>
+          <Mask
+            mask={
+              <Tile rect={TILE}>
+                <PhosphorDot channel="g" />
+              </Tile>
+            }
+          >
+            <Group>
+              <ColorMatrix matrix={filterColor(0, 1, 0)} />
+              {children}
+            </Group>
+          </Mask>
+          <Mask
+            mask={
+              <Tile rect={TILE}>
+                <PhosphorDot channel="b" />
+              </Tile>
+            }
+          >
+            <Group>
+              <ColorMatrix matrix={filterColor(0, 0, 1)} />
+              {children}
+            </Group>
+          </Mask>
+        </Group>
+      </Refresh>
+    </RemotionCanvas>
+  );
+};
+
+export const CRT = () => {
+  return (
+    <CRTEffect>
+      <Reference />
+    </CRTEffect>
+  );
+};
 
 // https://fecolormatrix.com/
 const filterColor = (r: number, g: number, b: number) => [
@@ -33,63 +105,3 @@ const filterColor = (r: number, g: number, b: number) => [
   1,
   0,
 ];
-
-interface CRTProps {
-  children: ReactNode | ReactNode[];
-}
-
-const CRTEffect = ({ children }: CRTProps) => {
-  return (
-    <RemotionCanvas
-      typefaces={[require("./assets/PublicPixel.ttf")]}
-      width={width}
-      height={height}
-    >
-      <Fill color="black" />
-      <Group>
-        <Mask
-          mask={
-            <Tile rect={TILE}>
-              <PhosphorDot channel="r" />
-            </Tile>
-          }
-        >
-          <Group>
-            <ColorMatrix matrix={filterColor(1, 0, 0)} />
-            {children}
-          </Group>
-        </Mask>
-        <Mask
-          mask={
-            <Tile rect={TILE}>
-              <PhosphorDot channel="g" />
-            </Tile>
-          }
-        >
-          <Group>
-            <ColorMatrix matrix={filterColor(0, 1, 0)} />
-            {children}
-          </Group>
-        </Mask>
-        <Mask
-          mask={
-            <Tile rect={TILE}>
-              <PhosphorDot channel="b" />
-            </Tile>
-          }
-        >
-          <Group>
-            <ColorMatrix matrix={filterColor(0, 0, 1)} />
-            {children}
-          </Group>
-        </Mask>
-      </Group>
-    </RemotionCanvas>
-  );
-};
-
-export const CRT = () => (
-  <CRTEffect>
-    <Reference />
-  </CRTEffect>
-);
